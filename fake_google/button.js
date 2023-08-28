91a52051633e04c141a23c2cf39772a1fa4d9418
佐藤蒼太 <satousouta@satousoutanoMacBook-Air.local>
2023-08-28 12:00:35 +0900

    変数urlの型をletからconstへ変更

--- fake_google/button.js
+++ fake_google/button.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('fakeButton');
 const fakeText = document.getElementById('fakeTextarea');
-let url = 'https://www.google.co.jp/search?q=';
+const url = 'https://www.google.co.jp/search?q=';
 
 button.addEventListener('click', function() {
-	window.open(url + fakeText.value);	
+    window.open(url + fakeText.value);	
 }, false);
